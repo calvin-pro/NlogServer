859f7e3109e7e516e6237abf7a0125a2c4c0cf91
qy z <calvinqyzhao@outlook.com>
2021-05-12 11:40:26 +0800

    添加项目文件。

---
+++ NlogServer/LogServer.cs
@@ -0,0 +1,99 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NlogServer
+{
+    public class LogServer
+    {
+        // Fields
+        private static readonly bool Isinit = false;
+        private static bool _logComplementEnable = false;
+        private static bool _logDubugEnable = false;
+        private static bool _logErrorEnable = false;
+        private static bool _logExceptionEnable = false;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static bool _logInfoEnable = false;
+
+        // Methods
+        static LogServer()
+        {
+            if (!Isinit)
+            {
+                Isinit = true;
+                SetConfig();
+            }
+        }
+
+
+        public static void SetConfig()
+        {
+            Logger enable = LogManager.GetLogger("con");//获取配置，设置是否允许输出
+            _logInfoEnable = enable.IsInfoEnabled;
+            _logErrorEnable = enable.IsErrorEnabled;
+            _logExceptionEnable = enable.IsErrorEnabled;
+            _logComplementEnable = enable.IsTraceEnabled;
+            _logDubugEnable = enable.IsDebugEnabled;
+        }
+
+        public static void WriteComplement(string info)
+        {
+            if (_logComplementEnable)
+            {
+                Logger.Trace(info);
+            }
+        }
+
+        public static void WriteComplement(string info, Exception ex)
+        {
+            if (_logComplementEnable)
+            {
+                Logger.Trace(ex,info);
+            }
+        }
+
+        public static void WriteDebug(string info)
+        {
+            if (_logDubugEnable)
+            {
+                Logger.Debug(info);
+            }
+        }
+
+        public static void WriteError(string info)
+        {
+            if (_logErrorEnable)
+            {
+                Logger.Error(info);
+            }
+        }
+
+        public static void WriteException(string info, Exception ex)
+        {
+            if (_logExceptionEnable)
+            {
+                Logger.Error(ex,info);
+            }
+        }
+
+        public static void WriteFatal(string info)
+        {
+            if (_logErrorEnable)
+            {
+                Logger.Fatal(info);
+            }
+        }
+
+        public static void WriteInfo(string info)
+        {
+            if (_logInfoEnable)
+            {
+                Logger.Info(info);
+            }
+        }
+    }
+}
---
+++ TestlogServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using NlogServer;
+using System;
+
+namespace TestlogServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogServer.WriteInfo("this info");
+            LogServer.WriteError("this Error");
+            LogServer.WriteDebug("this Debug");
+            LogServer.WriteFatal("this Fatal");
+        }
+    }
+}
